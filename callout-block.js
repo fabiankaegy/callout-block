377918c2ab181e5d7c0ea3a67adad5564ca58a0d
fabiankaegy <fabian@arvernus.info>
2019-02-07 23:33:42 +0100

    Add setting to change text & background color

--- callout-block.js
+++ callout-block.js
@@ -13,23 +13,71 @@ registerBlockType( 'jsforwp/callout-block', {
 		content: {
 			source: 'html',
 			selector: 'h5',
-		},
+        },
+        backgroundColor: {
+            type: 'string',
+            default: '#900900',
+        },
+        textColor: {
+            type: 'string',
+            default: '#ffffff',
+        }
 	},
 
 	edit: function( props ) {
-		return wp.element.createElement( wp.editor.RichText, {
-			tagName: 'h5',
-			className: props.className,
-			value: props.attributes.content,
-			onChange: function( content ) {
-				props.setAttributes( { content: content } );
-			}
-		} );
+		return createElement( 
+            wp.element.Fragment, 
+            null, 
+            createElement(
+                wp.editor.InspectorControls, 
+                null,
+                createElement(
+                    wp.editor.PanelColorSettings, {
+                        title: __("Color Settings", "jsforwp-callout-block"),
+                        colorSettings: [
+                            {
+                                value: props.attributes.backgroundColor,
+                                onChange: function( value ) {
+                                    props.setAttributes({ backgroundColor: value });
+                                },
+                                label: __("Background Color", "jsforwp-callout-block")
+                            },
+                            {
+                                value: props.attributes.textColor,
+                                onChange: function( value ) {
+                                    props.setAttributes({ textColor: value });
+                                },
+                                label: __("Text Color", "jsforwp-callout-block")
+                            }
+                        ]
+                    }
+                )
+            ),
+            createElement( 
+                wp.editor.RichText, {
+                    tagName: 'h5',
+                    className: props.className,
+                    value: props.attributes.content,
+                    style: {
+                        backgroundColor: props.attributes.backgroundColor,
+                        color: props.attributes.textColor
+                    },
+                    onChange: function( content ) {
+                        props.setAttributes( { content: content } );
+                    }
+                } 
+            ) 
+        );
 	},
 
 	save: function( props ) {
-		return wp.element.createElement( wp.editor.RichText.Content, {
-			tagName: 'h5', value: props.attributes.content
-		} );
+		return createElement( wp.editor.RichText.Content, {
+            tagName: 'h5', 
+            value: props.attributes.content,
+            style: {
+                backgroundColor: props.attributes.backgroundColor,
+                color: props.attributes.textColor
+            },		
+        } );
 	}
 } );
